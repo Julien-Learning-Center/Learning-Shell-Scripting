7caba3212393ba8af8f8bcd6475c515c76689808
Julien Broyard <broyard.dev@gmail.com>
2021-03-18 12:45:42 +0100

    Add files

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This repository holds the script I made while learning shell scripting.
 
 - [Arguments](scripts/arguments.sh)
 - [Conditional](scripts/conditional.sh)
+- [Files](scripts/files.sh)
 - [Math](scripts/math.sh)
 - [String Comparison](scripts/string_comparison.sh)
 - [Variables](scripts/variables.sh)
---
+++ scripts/files.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# --- Files ---
+
+if [ $# -ne 1 ]; then
+    echo "This script needs one argument to work correctly."
+    exit 1
+fi
+
+FILE=$1
+
+# Checks if the given file exists
+if [ -f "$FILE" ]; then
+    # Default variables
+    VAR_EXE="NO"
+    VAR_READ="NO"
+    VAR_WRITE="NO"
+
+    # Checks if the file is executable
+    if [ -x "$FILE" ]; then
+        VAR_EXE="YES"
+    fi
+
+    # Checks if the file is readable
+    if [ -r "$FILE" ]; then
+        VAR_READ="YES"
+    fi
+
+    # Checks if the file is writable
+    if [ -w "$FILE" ]; then
+        VAR_WRITE="YES"
+    fi
+
+    echo "===FILE: $FILE==="
+    echo "EXECUTABLE: $VAR_EXE"
+    echo "READ: $VAR_READ"
+    echo "WRITE: $VAR_WRITE"
+else
+    if [ -d "$FILE" ]; then
+        echo "$FILE is a directory."
+    else
+        echo "The given file does not exists."
+    fi
+
+    exit 2
+fi
